ca316c4d5d087762764979a8409024cd3dd405b7
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-08-03 20:04:22 -0700

    Update pretrain_dataset.py

--- data/pretrain_dataset.py
+++ data/pretrain_dataset.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset
 from data.utils import pre_caption
 import glob
+import numpy as np
 
 class GigaSpeech(Dataset):
     def __init__(self, hf_dataset, fea_dir, max_words=50):
@@ -29,7 +30,7 @@ class GigaSpeech(Dataset):
         caption = self.dataset["text_description"+str(tag)][index]
         caption = self.clean(caption)
         caption = pre_caption(caption, self.max_words)
-        hubert_fea = torch.load(os.path.join(self.fea_dir, segment_id+'.pt'))
+        hubert_fea = torch.from_numpy(np.load(os.path.join(self.fea_dir, segment_id+'.npz'))['arr_0'])
         return segment_id, hubert_fea, caption
 
 if __name__ == '__main__':
@@ -39,4 +40,3 @@ if __name__ == '__main__':
     dataset = GigaSpeech(hf_dataset, fea_dir)
     print(len(dataset))
     print(dataset[4])
-
