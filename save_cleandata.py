b97cbe311fab60e0f9b1dea363a6ad798649bfff
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-08-24 11:04:46 -0700

    Create save_cleandata.py

---
+++ save_cleandata.py
@@ -0,0 +1,15 @@
+from datasets import load_from_disk
+
+cache_dir = "/data/lmorove1/hwang258/sc/Speech-Captioning-Dataset/cache/out/gigaspeech-tiny-train"
+target_path = "/data/lmorove1/hwang258/sc/sc/BLIP/output/gigaspeech-tiny-train.txt"
+save_dir = "/data/lmorove1/hwang258/sc/Speech-Captioning-Dataset/cache/out/gigaspeech-tiny-train-clean"
+
+savenames = []
+with open(target_path, 'r') as file:
+    for line in file:
+        savenames.append(line.strip())
+        
+hf_dataset = load_from_disk(cache_dir)
+filtered_dataset = hf_dataset.filter(lambda example: example['segment_id'] in savenames)
+
+filtered_dataset.save_to_disk(save_dir)
